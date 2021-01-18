caa704460e9a407365a30ea490f2e24d1b78fa5f
VitaMinKin <inetmanager2009@gmail.com>
2021-01-18 14:46:28 +1000

    work on the page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,29 +2,209 @@
 
 @section('content')
 <header>
-    <div class="nav bg-primary text-white d-flex justify-content-center">
-        <p class="text-center align-content-center screenTitle">
-            ЭКРАН СОСТОЯНИЯ ВВОДА ОБЪЕКТОВ ИНФОРМАТИЗАЦИИ В ЭКСПЛУАТАЦИЮ
+    <div class="nav scrColor text-white d-flex justify-content-center rounded m-2">
+        <p class="text-center text-uppercase align-content-center my-2">
+            Экран состояния ввода объектов информатизации в эксплуатацию
         </p>
     </div>
 </header>
 <div class="flex-grow-1">
-    <div class='jumbotron jumbotron-fluid bg-dark'>
-        <div class='container-lg'>
+    <div class='container-fluid'>
             <div class='row'>
-                <div class='col-12 col-md-10 col-lg-8 mx-auto text-white'>
-                        <h1 class='display-3'>Page Analyzer</h1>
-                        <p class='lead'>Check web pages for free</p>
-                        <form class='d-flex justyfy-content-center' type="post" actions="/domains">
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control" name='domains[name]' placeholder="www.example.com" value="">
-                                <button class="btn btn-primary ml-3 px-5 text-uppercase" type="submit">Check</button>
+                <div class="col-sm mx-2 p-2 rounded text-white scrColor-transparent">
+                    <div class="row headText text-center align-items-center px-2">
+                        <div class="col-2">
+                            Ответственный за ОБИ
+                        </div>
+                        <div class="col-2">
+                            Управление
+                        </div>
+                        <div class="col-1">
+                            ОВТ
+                        </div>
+                        <div class="col-7">
+                            Прогресс ввода ОИ в эксплуатацию
+                        </div>
+                    </div>
+                    <div class="container-fluid bg-light text-dark rounded p-1 my-1">
+                        <table class="table table-sm table-bordered m-0">
+                            <tr>
+                                <td rowspan="4" class="col-2">
+                                    Фото 
+                                </td>
+                                <td class="col-2">
+                                    Управление
+                                </td>
+                                <td class="col-1">
+                                    ОВТ
+                                </td>
+                                <td class="col-7">
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr>   
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                <td>
+                                    Фото
+                                </td>
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr>  
+                        </table>
+                    </div>
+                    <!-- Для наглядности 
+                    <div class="container-fluid bg-light text-dark rounded p-1 my-1">
+                        <table class="table table-sm table-bordered m-0">
+                            <tr>
+                                <td rowspan="4" class="col-2">
+                                    Фото 
+                                </td>
+                                <td class="col-2">
+                                    Управление
+                                </td>
+                                <td class="col-1">
+                                    ОВТ
+                                </td>
+                                <td class="col-7">
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr>   
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr> 
+                            <tr>
+                                <td>
+                                    Фото
+                                </td>
+                                <td>
+                                    Управление
+                                </td>
+                                <td>
+                                    ОВТ
+                                </td>
+                                <td>
+                                    Прогресс ввода ОИ в эксплуатацию
+                                </td>
+                            </tr>  
+                        </table>
+                    </div>
+                    <!-- удалять здесь -->
+
+                </div>
+
+                <div class="col-sm mx-2">
+                    <div class="row scrColor-transparent rounded text-white align-items-center text-center">
+                        <div class="col">
+                            <div class="text-uppercase">
+                                Обобщенные сведения ОИ <span>2 кат. </span> управления <span> ПВО и А </span>
                             </div>
-                        </form>
+                            <div class="row">
+                                <div class="col-1">
+                                    x
+                                </div>
+                                <div class="col-5">
+                                    Наименование отработанного документа
+                                </div>
+                                <div class="col-1">
+                                    №
+                                </div>
+                                <div class="col-2">
+                                   0000
+                                </div>
+                                <div class="col-1">
+                                   от
+                                </div>
+                                <div class="col-2">
+                                   00.00.0000
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="row bg-light">
+                        <div class="col">
+                            Нижняя
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     </div>
+    
 </div>
 
 @endsection
