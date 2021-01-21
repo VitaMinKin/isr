8b606aca3335f5c0614feb61387e0d911cbb18b1
VitaMinKin <inetmanager2009@gmail.com>
2021-01-21 14:55:54 +1000

    Design of tables, added tables for officers, departments and objects of informatization

--- database/migrations/2021_01_19_132501_create_informatization_objects_table.php
+++ database/migrations/2021_01_19_132501_create_informatization_objects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateInformatizationObjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('informatization_objects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->smallInteger('category');
+            $table->foreignId('force_managment_id')->constrained()->onDelete('cascade');
+            $table->boolean('type');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('informatization_objects');
     }
 }
