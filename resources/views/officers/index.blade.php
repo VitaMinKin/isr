f9661516c62e3bf1437aa09fb629e63d772ab42d
VitaMinKin <inetmanager2009@gmail.com>
2021-02-21 15:12:14 +1000

    minor changes in views table officers

--- resources/views/officers/index.blade.php
+++ resources/views/officers/index.blade.php
@@ -5,14 +5,14 @@
     
     <h2 class="text-center text-uppercase mt-3"> Ответственные за обеспечение безопасности информации <br/> на объектах информатизации </h2>
     
-    <table class="table table-bordered table-striped table-responsive-sm" cols="5">
+    <table class="table table-bordered table-striped table-responsive-sm table-sm" cols="5">
     <thead class="table-dark">
         <tr class="text-center">
-        <th scope="col" class="col-1">№ п/п</th>
-        <th scope="col">Должность</th>
-        <th scope="col" class="col-2">Воинское звание</th>
-        <th scope="col">ФИО</th>
-        <th scope="col">Фото</th>
+        <th scope="col" width=3%>№ п/п</th>
+        <th scope="col" class='align-middle'>Должность</th>
+        <th scope="col" class="col-xl-2 col-2 align-middle">Воинское звание</th>
+        <th scope="col" class="align-middle">ФИО</th>
+        <th scope="col" class="align-middle">Действия</th>
         </tr>
     </thead>
     <tbody class="table-hover cursor-pointer">
@@ -26,11 +26,11 @@
         @else
        
             @foreach($officers as $officer)
-                <tr>
+                <tr title='{{ $officer->surname }} {{ $officer->name }} {{ $officer->patronymic }}'>
                     <th scope="row">{{ $loop->iteration }}</th>
                     <td>{{ $officer->military_position }} </td>
                     <td>{{ $officer->militaryRanks()->first()->name }}</td>
-                    <td><a href="/officers/{{ $officer->id }}">{{ $officer->surname }}</a></td>
+                    <td><a href="/officers/{{ $officer->id }}">{{ $officer->surname }} {{ mb_substr($officer->name, 0, 1) }}.{{ mb_substr($officer->patronymic, 0, 1) }}.</a></td>
                     <td>
                         <a href='{{ url("/officers/$officer->id/edit") }}'>Изменить</a>
                         <a href='{{ url("/officers/$officer->id/") }}' data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
