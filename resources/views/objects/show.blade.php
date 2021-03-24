02612f3627d70dabedc4b8d7646368f49d4554fc
VitaMinKin <inetmanager2009@gmail.com>
2021-03-24 14:38:18 +1000

    Changed object show vew page

--- resources/views/objects/show.blade.php
+++ resources/views/objects/show.blade.php
@@ -89,10 +89,10 @@
                                                     <p class="text-muted m-0 p-0">Предварительный учет <br /> (жувд)</p>
                                                 </div>
                                                 <div class="col">
-                                                    <p class="text-muted m-0 p-0">Постоянный учет <br />(инвентарный, входящий)</p>
+                                                    <p class="text-muted m-0 p-0">Дата документа</p>
                                                 </div>
                                                 <div class="col">
-                                                    <p class="text-muted m-0 p-0">Дата документа</p>        
+                                                    <p class="text-muted m-0 p-0">Срок действия</p>        
                                                 </div>
                                                 <div class="col">
                                                     <p class="text-muted m-0 p-0">Ссылка на файл</p>        
@@ -102,59 +102,78 @@
                                         @foreach ($object->documents as $document)
                                             <div class="list-group-item list-group-item-action">
                                                 <div class="row .bg-light">
-                                                    <!-- Trash label -->
+                                                    <!-- row with document name -->
                                                                                          
                                                     <div class="col">
                                                         <h6 class="card-title mb-2 font-weight-bold">
-                                                            <a href="{{ route('documents.edit', $document) }}" data-toggle="tooltip" data-placement="bottom" title="Редактировать"> {{ $document->documentName->title }}
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 20">
-                                                                <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                                                            </svg>
-                                                            </a>
-                                                            
-                                                            <a href="{{ route('documents.destroy', $document) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow" class="float-right" data-toggle="tooltip" data-placement="bottom" title="Удалить">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                                            </svg>
-                                                        </a>
+                                                            <a href="{{ route('documents.edit', $document) }}" data-toggle="tooltip" data-placement="bottom" title="Редактировать"> {{ $document->documentName->title }} </a>
                                                         </h6>
                                                     </div>
                                                                                    
                                                 </div>
 
                                                 <div class="row">
-                                                    <div class="col border-right">
-                                                        {{ $document->preliminary_accounting }}
+                                                    <div class="col border-right d-flex align-self-center justify-content-center">
+                                                        <div>
+                                                            <span class="font-weight-bold"> Предварительный учет: </span><br /> {{ $document->preliminary_accounting ?? "-" }} <br />
+                                                            <span class="font-weight-bold"> Постоянный учет: </span> <br /> 
+                                                            @if ($document->number_type == "inventory")
+                                                                Инв. 
+                                                            @elseif ($document->number_type == "inbox")
+                                                                Вх. 
+                                                            @endif
+                                                            {{ $document->number }}
+                                                            @if ($document->number_mil_unit == "osk")
+                                                                - ОСК ВВО
+                                                            @else
+                                                                - {{ $document->number_mil_unit ?? null}}
+                                                            @endif
+                                                        </div>
                                                     </div>
-                                                    <div class="col border-right">
-                                                    
-                                                        @if ($document->number_type == "inventory")
-                                                            Инв. 
-                                                        @elseif ($document->number_type == "inbox")
-                                                            Вх. 
-                                                        @endif
-                                                        {{ $document->number }}
-                                                        @if ($document->number_mil_unit == "osk")
-                                                            - ОСК ВВО
-                                                        @else
-                                                            - {{ $document->number_mil_unit ?? null}}
-                                                        @endif
+                                                    <div class="col border-right d-flex align-self-center justify-content-center">
+                                                        {{ $document->date ?? "Документ не готов (проект)"}}
                                                     </div>
-                                                    <div class="col border-right">
-                                                        {{ $document->date }}
+                                                    <div class="col border-right d-flex align-self-center justify-content-center">
+                                                       <!-- Срок действия -->
+                                                        Бессрочно
                                                     </div>
-                                                    <div class="col">
+                                                    <div class="col d-flex align-self-center justify-content-center">
                                                         @if ($document->file_name)
                                                             {{ Html::link(route('documents.download', $document), "$document->file_name") }}
                                                         @else
-                                                        <p class="text-muted m-0 p-0">Нет файла</p>
+                                                            <p class="text-muted m-0 p-0">Нет файла</p>
                                                         @endif
                                                     </div>
+                                                   
                                                 </div>
                                                 <div class="row">
                                                     <div class="col text-italic">
                                                         {{ $document->comment }}
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col d-flex justify-content-end">
+
+                                                        <a href="{{ route('documents.destroy', $document) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow" data-toggle="tooltip" data-placement="bottom" title="Удалить" class="mx-1">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                                            </svg>
+                                                        </a>
+                                                    
+                                                        <a href="{{ route('documents.edit', $document) }}" data-toggle="tooltip" data-placement="bottom" title="Редактировать" class="mx-1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                                            <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                                                        </svg>
+                                                        </a>
+                                                        
+                                                        <a href="#" data-toggle="tooltip" data-placement="bottom" title="Комментировать" class="mx-1">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                                                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                                            <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                                            </svg>
+                                                        </a>
+
                                                     </div>
                                                 </div>
                                             </div>
